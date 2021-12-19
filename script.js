381970043828873d0f743bfa5df97feae8a5750c
DIYORBECK <nazokatuz@mail.ru>
2021-12-20 04:55:27 +0500

    begin

---
+++ script.js
@@ -0,0 +1,23 @@
+var str = prompt('Введите свое имя')
+console.log('Меня зовут ' + str);
+
+var str = prompt('Введите свой возраст')
+console.log('Мне ' + str);
+
+var str = prompt('Решите пример 6+3=?')
+console.log('Пример 1: 6+3= '+(6+3) + ' Ваш ответ ' + str);
+
+var str = prompt('Решите пример 10-4=?')
+console.log('Пример 2: 10-4='+(10-4) + ' Ваш ответ ' + str);
+
+var str = prompt('Решите пример 48*3=?')
+console.log('Пример 3: 48*3= '+(48*3) +' Ваш ответ ' + str);
+
+var str = prompt('Решите пример 15/3=?')
+console.log('Пример 4: 15/3= '+(15/3) +' Ваш ответ ' + str);
+
+var str = prompt('Решите пример 10%3=?')
+console.log('Пример 5: 10%3= '+(10%3) +' Ваш ответ ' +str);
+
+var Name= 'Откройте консоль для просмотра'
+alert(Name)
